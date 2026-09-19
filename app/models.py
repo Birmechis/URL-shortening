9649f15e3e3cdb2195667b1dace88748c2f8baf5
Birmechis Mulugeta <birmechis.m@gmail.com>
2026-09-19 22:02:32 +0300

    Enforce required fields with nullable=False and ensure short_code is unique

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class ShortURL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, nullable=False)
-    shortCode = db.Column(db.String(20), nullable=False)
+    shortCode = db.Column(db.String(20), unique=True, nullable=False)
     createdAt = db.Column(
         db.DateTime,
         default=lambda: datetime.now(timezone.utc),
@@ -16,7 +16,7 @@ class ShortURL(db.Model):
         onupdate=lambda: datetime.now(timezone.utc),
         nullable=False
     )
-    accessCount = db.Column(db.Integer, nullable=False)
+    accessCount = db.Column(db.Integer, default=0, nullable=False)
 
     def __repr__(self):
         return f"<ShortURL {self.shortCode}>"
