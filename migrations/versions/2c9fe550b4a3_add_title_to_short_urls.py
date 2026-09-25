0549ac914620550189daff39575487eaedf43609
Birmechis Mulugeta <birmechis.m@gmail.com>
2026-09-25 13:54:03 +0300

    add initial database migration

---
+++ migrations/versions/2c9fe550b4a3_add_title_to_short_urls.py
@@ -0,0 +1,25 @@
+"""initial schema
+
+Revision ID: 2c9fe550b4a3
+Revises:
+Create Date: 2026-09-25 13:26:33.961772
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "2c9fe550b4a3"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
