01f0367adb1af0b8e5a2693ebf3c1bb0fadacecd
Birmechis Mulugeta <birmechis.m@gmail.com>
2026-09-26 18:58:33 +0300

    feat: add Redis client extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,22 @@
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
+from flask_migrate import Migrate
 from flask_limiter.util import get_remote_address
+import redis
+
+db = SQLAlchemy()
 
 limiter = Limiter(
     key_func=get_remote_address,
 )
 
+migrate = Migrate()
+
 jwt = JWTManager()
+
+def create_redis_client(redis_url):
+    return redis.Redis.from_url(
+        redis_url,
+        decode_responses=True,
+    )
